66d09267278c4fcac73ad52548a18c710fd03aa1
Shinnya01 <carreon.carll@gmail.com>
2025-08-10 19:18:03 +0800

    fix transport remove 'wss'

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -10,5 +10,5 @@ window.Echo = new Echo({
     wsPort: import.meta.env.VITE_REVERB_PORT,
     wssPort: import.meta.env.VITE_REVERB_PORT,
     forceTLS: import.meta.env.VITE_REVERB_USE_TLS === 'true',
-    enabledTransports: ['ws', 'wss'],
+    enabledTransports: ['ws'],
 });
