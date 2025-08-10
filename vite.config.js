7225d5b875695b592ddcbaccdc8f604206454c5e
Shinnya01 <carreon.carll@gmail.com>
2025-08-10 14:59:24 +0800

    fixed

--- vite.config.js
+++ vite.config.js
@@ -33,5 +33,14 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  server: {
+  host: '0.0.0.0',
+  port: 5173,
+  hmr: {
+    host: 'dlchub-livewire-production-650b.up.railway.app',
+    protocol: 'wss'
+  }
+}
+
   // Remove server config for production
 });
