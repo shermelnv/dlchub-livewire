2a92f00af86d29bfe825f1bdb0c8f28b8fdd6c08
Shinnya01 <carreon.carll@gmail.com>
2025-08-10 14:48:47 +0800

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,37 @@
-import {
-    defineConfig
-} from 'vite';
+// import {
+//     defineConfig
+// } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//         tailwindcss(),
+//     ],
+//     server: {
+//     host: '0.0.0.0',            // Allow external devices to connect
+//     port: 5173,                 // Default Vite port
+//     hmr: {
+//       host: '192.168.100.6',    // Your local IP (same as in APP_URL)
+//     },
+//   },
+// });
+
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-    host: '0.0.0.0',            // Allow external devices to connect
-    port: 5173,                 // Default Vite port
-    hmr: {
-      host: '192.168.100.6',    // Your local IP (same as in APP_URL)
-    },
-  },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
+  // Remove server config for production
 });
