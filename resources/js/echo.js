8fa7cb150444bff580e494e7c5fd2250a5352855
Shinnya01 <carreon.carll@gmail.com>
2025-09-15 18:46:12 +0800

    to pusher

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,3 +1,19 @@
+// import Echo from 'laravel-echo';
+
+// import Pusher from 'pusher-js';
+// window.Pusher = Pusher;
+
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: 'ogrjdpliov87qeaaw9gc',
+//     wsHost: 'plchubreverb-dokuv.ondigitalocean.app',
+//     wsPort: 6001,
+//     wssPort: 6001,
+//     forceTLS: true,
+//     enabledTransports: ['ws', 'wss'],
+// });
+
+
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
