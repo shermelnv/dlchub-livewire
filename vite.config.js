6d2a5e9c591677181e7e06174c609002d4e1c59b
Shinnya01 <carreon.carll@gmail.com>
2025-09-16 00:30:11 +0800

    fixed port

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     host: '0.0.0.0',            // Allow external devices to connect
     port: 5173,                 // Default Vite port
     hmr: {
-      host: '127.0.0.1',   
+      // host: '127.0.0.1',   
       // host: '192.168.100.27', // Your local IP (same as in APP_URL)
     },
   },
