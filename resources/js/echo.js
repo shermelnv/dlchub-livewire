8cab566344fffba3ef1f3695d510a7ac2af0e8d6
Shinnya01 <carreon.carll@gmail.com>
2025-09-15 19:03:58 +0800

    to pusher

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -20,11 +20,12 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: 'ogrjdpliov87qeaaw9gc',
-    wsHost: 'plchubreverb-dokuv.ondigitalocean.app',
-    wsPort: 6001,
-    wssPort: 6001,
+    broadcaster: "pusher",
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true,
-    enabledTransports: ['ws', 'wss'],
+    wsHost: import.meta.env.VITE_PUSHER_HOST,
+    wsPort: import.meta.env.VITE_PUSHER_PORT,
+    wssPort: import.meta.env.VITE_PUSHER_PORT,
+    enabledTransports: ["ws", "wss"],
 });
