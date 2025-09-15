6c14a1c0d720dab2c6d843450b0e3378c87373ca
Shinnya01 <carreon.carll@gmail.com>
2025-09-15 18:17:01 +0800

    echo changed

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -5,10 +5,10 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    key: 'ogrjdpliov87qeaaw9gc',
+    wsHost: 'plchubreverb-dokuv.ondigitalocean.app',
+    wsPort: 6001,
+    wssPort: 6001,
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
