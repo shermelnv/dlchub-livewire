d31e3a8b4f36f72e1b99489c337a254a1fcffe7d
Shinnya01 <carreon.carll@gmail.com>
2025-09-15 23:27:42 +0800

    ui fix dashboard

--- resources/views/livewire/dashboard-recent-activity.blade.php
+++ resources/views/livewire/dashboard-recent-activity.blade.php
@@ -17,7 +17,7 @@
         <p>Recent Activities</p>
         <div class="flex gap-2 items-center">
             <flux:input placeholder="Search" icon="magnifying-glass" clearable size="sm" wire:model.live="search" />
-            <!-- Filter -->
+            
             <flux:dropdown>
                 <flux:button icon:trailing="chevron-down" size="sm">
                     {{ $filterType ? ucfirst($filterType) : 'All' }}
@@ -35,15 +35,15 @@
         </div>
     </div>
 
-    <div class="flex-1 max-h-[60vh] overflow-y-auto text-sm text-gray-600 dark:text-gray-300 bg-zinc-900 rounded-lg relative">
+    <div class="flex-1 max-h-[60vh] overflow-y-auto text-sm text-gray-600 dark:text-gray-300 bg-zinc-100 dark:bg-zinc-900 rounded-lg relative">
 
     <table class="w-full text-left border-collapse">
-        <thead class="bg-neutral-100 dark:bg-zinc-700 text-xs uppercase sticky top-0">
+        <thead class="bg-zinc-100 dark:bg-zinc-700 uppercase sticky text-sm lg:text-lg top-0">
             <tr>
                 <th class="px-3 py-2">Type</th>
                 <th class="px-3 py-2">Activity</th>
                 <th class="px-3 py-2">Date</th>
-                <th class="px-3 py-2">Time</th>
+                <!--<th class="px-3 py-2">Time</th>-->
                 <th class="px-3 py-2">Status</th>
             </tr>
         </thead>
@@ -51,20 +51,20 @@
                     @forelse ($activities as $activity)
                         <tr >
                             <!-- Type -->
-                            <td class="px-3 py-2 capitalize">{{ $activity->type }}</td>
+                            <td class="px-3 py-2 text-xs lg:text-base capitalize">{{ $activity->type }}</td>
 
                             <!-- Message -->
-                            <td class="px-3 py-2">{{ $activity->message }}</td>
+                            <td class="px-3 py-2 text-xs lg:text-base">{{ $activity->message }}</td>
 
                             <!-- Date -->
-                            <td class="px-3 py-2">
-                                {{ $activity->created_at->format('d M, Y') }}
+                            <td class="px-3 py-2 text-xs lg:text-base">
+                                {{ $activity->created_at->format('d M, Y h:i A') }}
                             </td>
 
                             <!-- Time -->
-                            <td class="px-3 py-2">
-                                {{ $activity->created_at->format('h:i A') }}
-                            </td>
+                            <!--<td class="px-3 py-2">-->
+                            <!--    {{ $activity->created_at->format('h:i A') }}-->
+                            <!--</td>-->
 
                             <!-- Status -->
                             <td class="px-3 py-2">
